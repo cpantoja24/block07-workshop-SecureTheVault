4bb6a9c5a0946ec8a2d048e3da504d2b24f8af35
Cheyenne Pantoja <Cheyenne@MBP-5000.frontiernet.net>
2023-11-06 11:45:24 -0800

    updated comments

--- secureTheVault.js
+++ secureTheVault.js
@@ -1,19 +1,20 @@
 // Create a str for the user with a message for the user with the combination
-// Create variable for the first number using an arithmetic operator 
-// Create variable for the second number using an arithmetic operator 
-// Create variable for the third number using an arithmetic operator 
+// Create variable for the first number using a unique arithmetic operator 
+// Create variable for the second number using a unique arithmetic operator 
+// Create variable for the third number using a unique arithmetic operator 
 
-// First number in the combination
+// First number in the combination using a unique arithmetic operator 
 const num1 = 11 - 1;
 
-//Second number in the combination
+//Second number in the combination using a unique arithmetic operator 
 const num2 = 20 * 2;
 
-//Third number in the combination
+//Third number in the combination using a unique arithmetic operator 
 const num3 = 30 + 9;
 
 // Message for the user with the combination
 const welcome = `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${num1} - ${num2} - ${num3}`;
 
-window.alert(welcome);
+//Display the message in html file
+document.write(welcome);
 
